26cb11fd172e603ed89cc12f6f1f1a74aae61cdc
Jimmy Phan <jimmyphan777@gmail.com>
2019-09-19 10:28:55 -0500

    first commit

---
+++ Driver.java
@@ -0,0 +1,11 @@
+package assignment2;
+
+public class Driver {
+    public static void main(String args[]){
+        // get testing mode
+
+        // create game
+
+        // run game
+    }
+}
---
+++ Game.java
@@ -0,0 +1,27 @@
+package assignment2;
+
+import java.util.Scanner;
+
+public class Game {
+    private int numOfGuesses;
+    private int testingMode;
+    private Scanner scan;
+    // Constructors
+    Game(){
+        
+    }
+
+
+    public void runGame(){
+        boolean gameOver = false;
+
+        while(!gameOver){
+
+
+
+
+            // check if game is over
+            gameOver = true;
+        }
+    }
+}
